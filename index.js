7c99355c66984bf7b41336b650c8a31e01698650
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-03-18 15:07:26 +0000

    yarn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const port = process.env.PORT || 8080
 const app = express()
+const path = require('path')
 
 app.use(express.static(`${__dirname}/dist`))
 
@@ -10,5 +11,12 @@ app.use(express.static(`${__dirname}/dist`))
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 // this says that any request that comes into any url, we will respond with our index html folder that contains the script tag to go to the bundle.js
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'src/build')))
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src/build', 'index.html'))
+  })
+}
 
 app.listen(port, () => console.log(`App is listening on port ${port}`))
